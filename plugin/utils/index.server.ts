06ed5f59b91bd12b2afc751387d8f2c983c4cdab
Nico Brinkkemper <nico@brinkkemper.nl>
2026-02-25 19:42:54 +0100

    fix: exclude callServer from server barrel (imports createCallServer)

--- plugin/utils/index.server.ts
+++ plugin/utils/index.server.ts
@@ -1,7 +1,6 @@
-// Server barrel: excludes browser-only modules (createReactFetcher, useRscHmr,
-// createCallServer) that import from react-server-dom-esm/client.browser or
-// use React hooks which fail under react-server condition (React CJS exports)
-export * from "./callServer.js";
+// Server barrel: excludes browser-only modules that import from
+// react-server-dom-esm/client.browser or use React hooks:
+//   - createReactFetcher, useRscHmr, createCallServer, callServer
 export * from "./urls.js";
 export * from "./env.js";
 export * from "./routeToURL.js";
